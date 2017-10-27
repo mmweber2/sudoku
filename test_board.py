ff91f9f09c5a32e112611a27cfca25fcc4ac05b1
Mary Ellen Weber <mmweber2@gmail.com>
2017-10-26 19:55:34 -0500

    Begin writing tests for numbers_in_X methods.

--- test_board.py
+++ test_board.py
@@ -1,3 +1,4 @@
+from mock import patch
 from board import Board
 from nose.tools import assert_raises
 from nose.tools import assert_equals
@@ -16,9 +17,7 @@ def test_is_valid_start_board_valid():
         [2, 0, 3, 0, 0, 0, 0, 6, 0],
         [1, 5, 0, 0, 6, 0, 0, 0, 0]
         ]
-    # The Board constructor raises a ValueError if the board is invalid
-    # _is_valid_start_board() called by constructor.
-    assert Board(input_array)
+    assert Board._is_valid_start_board(input_array)
 
 def test_is_valid_start_board_size_4():
     input_array = [
@@ -27,16 +26,16 @@ def test_is_valid_start_board_size_4():
         [2, 0, 1, 0],
         [0, 0, 0, 0]
     ]
-    assert Board(input_array)
+    assert Board._is_valid_start_board(input_array)
 
 def test_is_valid_start_board_size_25_empty():
     input_array = [[0] * 25 for _ in xrange(25)]
-    assert Board(input_array)
+    assert Board._is_valid_start_board(input_array)
 
 def test_is_valid_start_double_digit_values():
     input_array = [[0] * 25 for _ in xrange(25)]
     input_array[0] = range(1, 26)
-    assert Board(input_array)
+    assert Board._is_valid_start_board(input_array)
 
 def test_board_constructor_valid():
     input_array = [
@@ -52,6 +51,12 @@ def test_board_constructor_valid():
                   ]
     b = Board(input_array)
     assert_equals(b.board, input_array)
+
+@patch('board.Board._is_valid_start_board', return_value=False)
+def test_board_constructor_invalid_start_board(board_mock):
+    with assert_raises(ValueError) as cm:
+        Board([])
+    assert_equals(str(cm.exception), "Invalid start board configuration.")
 
 def test_is_valid_start_board_wrong_outer_size():
     input_array = [[1, 0], [0, 0]]
@@ -83,7 +88,6 @@ def test_is_valid_start_board_no_inner_list():
 # For now, an empty board is valid
 def test_is_valid_start_board_empty():
     b = Board(([[0] * 9 for _ in xrange(9)]))
-    # Constructor contains 
     assert b
 
 def test_is_valid_start_board_non_int():
@@ -169,7 +173,7 @@ def test_is_valid_board_duplicate_in_box():
     assert not b._is_valid_board()
 
 # The _numbers_in_row tests don't need checking for duplicates because
-# that will be done when the board is created.
+# that will be done as part of _is_valid_board.
 def test_numbers_in_row_valid():
     input_array = [
                    [0, 0, 0, 0, 9, 0, 0, 5, 2],
@@ -203,8 +207,7 @@ def test_numbers_in_row_empty():
     b = Board(input_array)
     assert_equals(b._numbers_in_row(0), set())
 
-def test_numbers_in_row_invalid_row():
-    input_array = [
+dummy_9_array = [
                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
                    [0, 1, 0, 0, 0, 0, 3, 0, 4],
                    [0, 0, 2, 3, 1, 5, 0, 0, 9],
@@ -215,8 +218,15 @@ def test_numbers_in_row_invalid_row():
                    [2, 0, 3, 0, 0, 0, 0, 6, 0],
                    [1, 5, 0, 0, 6, 0, 0, 0, 0]
                   ]
-    b = Board(input_array)
-    assert_raises(IndexError, b._numbers_in_row, 9)
+dummy_9_board = Board(dummy_9_array)
+# TODO: Add dummy boards for other sizes and parameterize
+
+# TODO: Parameterize these tests for all board sizes
+def test_numbers_in_row_out_of_range(test_board):
+    assert_raises(IndexError, test_board._numbers_in_row, 9)
+
+def test_numbers_in_row_non_integer(test_board):
+    assert_raises(IndexError, test_board._numbers_in_row, "test")
 
 def test_numbers_in_col_valid():
     input_array = [
@@ -233,20 +243,12 @@ def test_numbers_in_col_valid():
     b = Board(input_array)
     assert_equals(b._numbers_in_column(8), set((4, 9)))
 
-def test_numbers_in_col_invalid_col():
-    input_array = [
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 1, 0, 0, 0, 0, 3, 0, 4],
-                   [0, 0, 2, 3, 1, 5, 0, 0, 9],
-                   [0, 0, 8, 7, 4, 6, 0, 3, 0],
-                   [0, 7, 0, 9, 0, 1, 0, 2, 0],
-                   [0, 9, 0, 2, 5, 3, 7, 0, 0],
-                   [0, 0, 0, 5, 3, 8, 2, 0, 0],
-                   [0, 0, 3, 0, 0, 0, 0, 6, 0],
-                   [0, 5, 0, 0, 6, 0, 0, 0, 0]
-                  ]
-    b = Board(input_array)
-    assert_raises(IndexError, b._numbers_in_column, "4")
+def test_numbers_in_col_out_of_range(test_board):
+    assert_raises(IndexError, test_board._numbers_in_column, 10)
+
+def test_numbers_in_col_non_numeric(test_board):
+    # Pass a string instead of int
+    assert_raises(IndexError, test_board._numbers_in_column, "4")
 
 def test_numbers_in_box_valid():
     input_array = [
@@ -263,20 +265,12 @@ def test_numbers_in_box_valid():
     b = Board(input_array)
     assert_equals(b._numbers_in_box(3, 3), set((7, 4, 6, 9, 1, 2, 5, 3)))
 
-def test_numbers_in_box_invalid_params():
-    input_array = [
-                   [0, 0, 0, 0, 9, 0, 0, 5, 2],
-                   [0, 1, 0, 0, 0, 0, 3, 0, 4],
-                   [0, 0, 2, 3, 1, 5, 0, 0, 9],
-                   [0, 0, 8, 7, 4, 6, 0, 3, 0],
-                   [0, 7, 0, 9, 0, 1, 0, 2, 0],
-                   [0, 9, 0, 2, 5, 3, 7, 0, 0],
-                   [4, 0, 0, 5, 3, 8, 2, 0, 0],
-                   [2, 0, 3, 0, 0, 0, 0, 6, 0],
-                   [1, 5, 0, 0, 6, 0, 0, 0, 0]
-                  ]
-    b = Board(input_array)
-    assert_raises(IndexError, b._numbers_in_box, 1, 1)
+# Parameters are in the box range, but are not border values
+def test_numbers_in_box_in_range_not_border(test_board):
+    assert_raises(IndexError, test_board._numbers_in_box, 1, 1)
+
+def test_numbers_in_box_non_number_parameters(test_board):
+    assert_raises(IndexError, test_board._numbers_in_box, "a", "b")
 
 def test_valid_moves_valid():
     input_array = [
