5affad0adb0fbf5bcbdfdcadb997054ef6a208cf
Mary Ellen Weber <mmweber2@gmail.com>
2018-01-30 15:31:34 -0600

    Remove recursion from count_solutions.
    Fix various bugs in fill_board.
    Add function _fill_simple.

--- solver.py
+++ solver.py
@@ -3,7 +3,6 @@ import copy
 
 # TODO: The board gets solved, but then visits the last position twice. Why?
 
-
 # A move has a row, column, and other options to play instead
 Move = namedtuple('Move', 'row col options')
 
@@ -25,79 +24,86 @@ def count_solutions(board):
     Returns:
         One of the integers 0, 1, or 2.
     """
-    # For now, don't worry about finding most constrained position
-    print "Called with board"
-    print board
+    # Fill in all single-solution positions and check for unwinnable positions
+    if not _fill_simple(board):
+        return 0 # Unwinnable position
     for row in xrange(9):
         for col in xrange(9):
-            solutions = 0
+            # Keep track of solutions from each position; there should be
+            #   multiple valid plays in different positions,
+            #   since _fill_simple handled the zero and single move cases.
+            solutions = 0 
             if board.board[row][col]: # Cell is not empty
                 continue
-            # At least one cell is open (end condition check)
-            print "Looking at position ", row, col
-            remaining = board.valid_moves(row, col)
-            print board
-            print "Remaining here is ", remaining
-            for move in remaining:
-                # Try all of these moves
-                board.board[row][col] = move
+            for move in board.valid_moves(row, col):
+                board_copy = copy.deepcopy(board)
                 # Make the move on this board, but don't keep all the changes
                 #    made by the recursive calls from here
-                print "Before, solutions is ", solutions
-                board_copy = copy.deepcopy(board)
-                solutions += count_solutions(board_copy)
-                print "After, solutions is ", solutions
-                # Stop iterating if more than one solution is found
+                board_copy.board[row][col] = move
+                if fill_board(board_copy):
+                    # If it's possible to fill the board after making this move,
+                    #   consider this move a valid solution
+                    solutions += 1
                 if solutions >= 2:
                     return solutions
-            return solutions # Will be 0 or 1
-    # Board is full
-    print "Board is done!"
+    # If solutions never reached more than 1, there is only one solution for the board
     return 1
 
-# TODO: Write function that runs a loop searching for cells with only one possibility
+def _fill_simple(board):
+    '''Fills all the spots on the board that only have one possible move.'''
+    # Keep checking the full board as long as progress is being made
+    while True:
+        set_value = False
+        for row, col in _find_empty_spots(board):
+            remaining = board.valid_moves(row, col)
+            if not remaining:
+                # Dead-end position
+                return None
+            elif len(remaining) == 1:
+                # Make move and continue
+                board.board[row][col] = remaining.pop()
+                set_value = True
+        if not set_value:
+            # The board is valid, but no more spaces can be filled this way
+            return board
 
 # Recursive move searcher and implementer
-# TODO: Is it better to use recursion or a stack?
 def fill_board(board):
     """
     Fully solves the board, if possible, and returns the result.
+
+    When generator is set to True, a random play will be chosen when there are multiple solutions.
 
     Returns:
         The solved board object if the board is solvable, None otherwise.
     """
     # Set a maximum value for moves remaining from a square
     smallest_move = float("inf")
-    next_moves = []
+    next_move = None
+    if not _fill_simple(board):
+        # Unwinnable board
+        return None
     for row, col in _find_empty_spots(board):
         remaining = board.valid_moves(row, col)
-        if not remaining:
-            # Dead-end position
-            return None
-        elif len(remaining) == 1:
-            # Make move and continue
-            board.board[row][col] = remaining.pop()
-            return fill_board(board)
-        else:
-            # Find most constrained position, if no 1s or 0s
-            if len(remaining) < smallest_move:
-                smallest_move = len(remaining)
-                next_moves.append(Move(row, col, remaining))
+        # Spots with fewer than 2 options are handled by _fill_simple,
+        # so pick any of the most constrained remaining positions
+        if len(remaining) < smallest_move:
+            smallest_move = len(remaining)
+            next_move = Move(row, col, remaining)
     # Check for won position; board must be valid because moves were
     #   chosen only from valid moves
-    if not next_moves:
+    if not next_move:
         # Pass up the winning board
         return board
-    # Otherwise, make one of the most constrained moves
-    for move in next_moves:
-        for option in move.options:
-            board.board[move.row][move.col] = option
-            # fill_board will attempt to fill the board passed to it.
-            # If it's not successful, it will contain moves that don't lead to
-            #   a valid solution, with no path to undoing them, so make a copy.
-            end_result = fill_board(copy.deepcopy(board))
-            # end_result will be None unless it is fully complete.
-            if end_result:
-                # Pass up the winning board
-                return end_result
+    for option in next_move.options:
+        board_copy = copy.deepcopy(board)
+        board_copy.board[next_move.row][next_move.col] = option
+        # fill_board will attempt to fill the board passed to it.
+        # If it's not successful, it will contain moves that don't lead to
+        #   a valid solution, with no path to undoing them, so make a copy.
+        end_result = fill_board(board_copy)
+        # end_result will be None unless it is fully complete.
+        if end_result:
+            # Pass up the winning board
+            return end_result
     return None
