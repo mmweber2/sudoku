b9ebfebce025be74bdf8e236eb70b8cbc48a9cea
Mary Ellen Weber <mmweber2@gmail.com>
2018-02-01 19:50:08 -0600

    Add test_count_solutions_unwinnable.

--- test_solver.py
+++ test_solver.py
@@ -181,6 +181,20 @@ def test_find_empty_spots_single():
                       ])
     assert_equals(_find_empty_spots(b), [(1, 3)])
 
+def test_find_empty_spots_edge_rows():
+    b = Board([
+                      [8, 9, 0, 1, 3, 4, 7, 5, 6],
+                      [7, 6, 3, 9, 5, 8, 2, 4, 1],
+                      [4, 5, 1, 2, 7, 6, 3, 8, 9],
+                      [6, 3, 8, 4, 9, 7, 1, 2, 5],
+                      [9, 2, 7, 5, 6, 1, 4, 3, 8],
+                      [5, 1, 4, 3, 8, 2, 9, 6, 7],
+                      [1, 7, 6, 8, 2, 3, 5, 9, 4],
+                      [2, 8, 9, 7, 4, 5, 6, 1, 3],
+                      [3, 4, 5, 6, 1, 9, 0, 7, 2]
+                      ])
+    assert_equals(_find_empty_spots(b), [(0, 2), (8, 6)])
+
 def test_count_solutions_done():
     assert_equals(count_solutions(board5_result), 1)
 
@@ -229,3 +243,17 @@ def test_count_solutions_empty():
 def test_count_solutions_unique():
     for b in (board1, board2, board3, board4, board5):
         assert_equals(count_solutions(b), 1)
+
+def test_count_solutions_unwinnable():
+    b = Board([
+                      [8, 9, 2, 1, 3, 4, 7, 0, 6],
+                      [7, 6, 3, 0, 5, 8, 2, 4, 1],
+                      [4, 5, 1, 2, 7, 6, 3, 8, 9],
+                      [6, 3, 8, 4, 9, 7, 1, 2, 5],
+                      [9, 2, 7, 5, 6, 1, 4, 3, 8],
+                      [5, 1, 4, 3, 8, 2, 9, 6, 7],
+                      [1, 7, 6, 8, 2, 3, 0, 9, 4],
+                      [2, 8, 9, 7, 4, 0, 6, 5, 3],
+                      [3, 4, 5, 6, 1, 9, 8, 7, 2]
+               ])
+    assert_equals(count_solutions(b), 0)
